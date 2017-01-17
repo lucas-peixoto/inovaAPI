6f735364b3534dcc12bef322c341e4ab29335457
Lucas Peixoto <lpeixoto2000@hotmail.com>
2017-01-17 19:48:24 -0300

    Creating route add

--- public/index.php
+++ public/index.php
@@ -30,7 +30,7 @@ $container['logger'] = function($c) {
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
     $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-        $db['user'], $db['pass']);
+    $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
@@ -41,6 +41,36 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     $response->getBody()->write("Hello, $name");
     $this->logger->addInfo("Name: $name");
 
+    return $response;
+});
+
+$app->post('/add', function (Request $request, Response $response) {
+    $this->logger->addInfo("entering /add route");
+
+    $data = $request->getParsedBody();
+
+    $aluno_data = [];
+    $aluno_data['nome'] = filter_var($data['nome'], FILTER_SANITIZE_STRING);
+    $aluno_data['turno'] = filter_var($data['turno'], FILTER_SANITIZE_STRING);
+    $aluno_data['email'] = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
+    $aluno_data['curso'] = filter_var($data['curso'], FILTER_SANITIZE_STRING);
+
+    $cursoMapper = new CursoMapper($this->db);
+
+    if ($cursoMapper->checkName($curso->getNome()) == -1) {
+        $this->logger->addInfo("Saving curso " . $aluno_data['curso']);
+
+        $curso = new CursoEntity($aluno_data['curso']);
+        $cursoMapper->save($curso);
+    }
+
+    $aluno = new AlunoEntity($aluno_data);
+    $alunoMapper = new AlunoMapper($this->db);
+
+    $this->logger->addInfo("Saving aluno " . $aluno->getNome());
+    $alunoMapper->save($aluno);
+
+    $response->getBody()->write("Ok");
     return $response;
 });
 
