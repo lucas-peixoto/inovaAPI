bb1ef963777d1bb1685ba80e0e1ae17b7d3ad292
lucas-peixoto <lucas.peixoto.14@gmail.com>
2017-01-31 20:43:54 -0300

    Route authenticate created

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ spl_autoload_register(function ($classname) {
     require ("../classes/" . $classname . ".php");
 });
 
+date_default_timezone_set('America/Fortaleza');
+
 $config['displayErrorDetails'] = true;
 // $config['addContentLengthHeader'] = false;
 
@@ -21,7 +23,7 @@ $container = $app->getContainer();
 $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
 
 $container['logger'] = function($c) {
-    $logger = new \Monolog\Logger('my_logger');
+    $logger = new \Monolog\Logger('logger');
     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
     $logger->pushHandler($file_handler);
     return $logger;
@@ -29,8 +31,8 @@ $container['logger'] = function($c) {
 
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-    $db['user'], $db['pass']);
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'] . ";charset=utf8",
+        $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
@@ -44,45 +46,23 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->get('/test', function (Request $request, Response $response) {
-    $aluno_data = [];
-    $aluno_data['nome'] = "Xicara";
-    $aluno_data['cpf'] = "756.376.109-01";
-    $aluno_data['email'] = "xica2k17@yahoo.com.br";
-    $aluno_data['telefone'] = "(88) 384635298";
-    $aluno_data['curso'] = "Engenharia Aero-Espacial";
-    $aluno_data['turno'] = "Manhã";
-    $endereco_data = [];
-    $endereco_data['rua'] = "Beco Diagonal";
-    $endereco_data['numero'] = "403";
-    $endereco_data['bairro'] = "Biboca";
-    $endereco_data['cidade'] = "Diagonal City";
-    $endereco_data['estado'] = "HG";
-    $endereco_data['cep'] = "63180-000";
+$app->post('/authenticate', function (Request $request, Response $response) {
+    $this->logger->addInfo("/authenticate");
+    $data = $request->getParsedBody();
 
-    $cursoMapper = new CursoMapper($this->db);
-    if ($cursoMapper->checkName($aluno_data['curso']) == -1) {
-        $this->logger->addInfo("Saving curso " . $aluno_data['curso']);
+    $username = filter_var($data['username'], FILTER_SANITIZE_STRING);
+    $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
 
-        $curso = new CursoEntity(["nome" => $aluno_data['curso']]);
-        $cursoMapper->save($curso);
+    $userMapper = new UserMapper($this->db);
+    $user = $userMapper->getUserByCredentials($username, $password, 'ARRAY');
+
+    if ($user) {
+        $resp = array( 'success' => 'true', 'token' => $user->getToken );
+    } else {
+        $resp = array( 'success' => 'false', 'msg' => 'Credenciais inválidas' );
     }
 
-    $endereco = new EnderecoEntity($endereco_data);
-    $enderecoMapper = new EnderecoMapper($this->db);
-    $this->logger->addInfo("Saving endereco " . $endereco->toString());
-    $endereco_id = $enderecoMapper->save($endereco);
-
-    $aluno_data['endereco_id'] = $endereco_id;
-
-    $aluno = new AlunoEntity($aluno_data);
-    $alunoMapper = new AlunoMapper($this->db);
-    $this->logger->addInfo("Saving aluno " . $aluno->getNome());
-    $alunoMapper->save($aluno);
-
-    $response->getBody()->write("Ok, I guess.");
-
-    return $response;
+    return $this->view->render($response, 'json.php', ["data" => $resp]);
 });
 
 $app->group('/get', function () use ($app) {
@@ -141,6 +121,47 @@ $app->post('/add', function (Request $request, Response $response) {
     $alunoMapper->save($aluno);
 
     $response->getBody()->write("Ok");
+    return $response;
+});
+
+$app->get('/test', function (Request $request, Response $response) {
+    $aluno_data = [];
+    $aluno_data['nome'] = "Xicara";
+    $aluno_data['cpf'] = "756.376.109-01";
+    $aluno_data['email'] = "xica2k17@yahoo.com.br";
+    $aluno_data['telefone'] = "(88) 384635298";
+    $aluno_data['curso'] = "Engenharia Aero-Espacial";
+    $aluno_data['turno'] = "Manhã";
+    $endereco_data = [];
+    $endereco_data['rua'] = "Beco Diagonal";
+    $endereco_data['numero'] = "403";
+    $endereco_data['bairro'] = "Biboca";
+    $endereco_data['cidade'] = "Diagonal City";
+    $endereco_data['estado'] = "HG";
+    $endereco_data['cep'] = "63180-000";
+
+    $cursoMapper = new CursoMapper($this->db);
+    if ($cursoMapper->checkName($aluno_data['curso']) == -1) {
+        $this->logger->addInfo("Saving curso " . $aluno_data['curso']);
+
+        $curso = new CursoEntity(["nome" => $aluno_data['curso']]);
+        $cursoMapper->save($curso);
+    }
+
+    $endereco = new EnderecoEntity($endereco_data);
+    $enderecoMapper = new EnderecoMapper($this->db);
+    $this->logger->addInfo("Saving endereco " . $endereco->toString());
+    $endereco_id = $enderecoMapper->save($endereco);
+
+    $aluno_data['endereco_id'] = $endereco_id;
+
+    $aluno = new AlunoEntity($aluno_data);
+    $alunoMapper = new AlunoMapper($this->db);
+    $this->logger->addInfo("Saving aluno " . $aluno->getNome());
+    $alunoMapper->save($aluno);
+
+    $response->getBody()->write("Ok, I guess.");
+
     return $response;
 });
 
