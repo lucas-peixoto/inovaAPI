ade89d77bc93c17ff3fe8a14f12ab4a9fd734f4d
lucas-peixoto <lucas.peixoto.14@gmail.com>
2017-02-01 14:46:08 -0300

    Updating CursoMapper getCursos() and getCursoById()

--- classes/CursoMapper.php
+++ classes/CursoMapper.php
@@ -19,26 +19,44 @@ class CursoMapper extends Mapper
         }
     }
 
-    public function getCursos() {
-        $sql = "SELECT id, nome FROM cursos";
+    public function getCursos($return_type = 'OBJECT') {
+        $sql = "SELECT c.id, c.nome, COUNT(a.id) as total
+            FROM cursos c
+            LEFT JOIN alunos a ON a.curso_id = c.id GROUP BY c.id";
+            
         $stmt = $this->db->query($sql);
         $results = [];
 
-        while($row = $stmt->fetch()) {
-            $results[] = new CursoEntity($row);
+        if ($return_type == 'OBJECT') {
+            while($row = $stmt->fetch()) {
+                $results[] = new CursoEntity($row);
+            }
+        } else if ($return_type == 'ARRAY') {
+            while($row = $stmt->fetch()) {
+                $results[] = $row;
+            }
         }
 
         return $results;
     }
 
-    public function getCursoById($id) {
-        $sql = "SELECT id, nome
-        from cursos where id = :curso_id";
+    public function getCursoById($curso_id, $return_type = 'OBJECT') {
+        $sql = "SELECT id, nome,
+        	(SELECT COUNT(*) FROM alunos WHERE turno = 'manha' AND curso_id = :curso_id) as turno_manha,
+        	(SELECT COUNT(*) FROM alunos WHERE turno = 'tarde' AND curso_id = :curso_id) as turno_tarde,
+        	(SELECT COUNT(*) FROM alunos WHERE turno = 'noite' AND curso_id = :curso_id) as turno_noite
+            FROM cursos WHERE id = :curso_id";
 
         $stmt = $this->db->prepare($sql);
-        $stmt->execute(["curso_id" => $id]);
+        $stmt->execute(["curso_id" => $curso_id]);
 
-        return new CursoEntity($stmt->fetch());
+        if($result) {
+            if ($return_type == 'OBJECT') {
+                return new CursoEntity($stmt->fetch());
+            } else if ($return_type == 'ARRAY') {
+                return $stmt->fetch();
+            }
+        }
     }
 
     public function save(CursoEntity $curso) {
